56561905183f36429318da2fd322b4ca0965290b
marina eleshaa <marinaeleshaa>
2025-11-16 14:07:59 +0200

    fix: remove duplicate children from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f6f6f6]`}
       >
-        <div className=" m-auto ">{children}</div>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ModeToggle/>
           {children}
