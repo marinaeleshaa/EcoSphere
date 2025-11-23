a3fc6487f111244e2c92ed5d481f8a72a9ea1e00
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:23:09 +0200

    refactor: use request dto and fromat the file

--- src/backend/features/auth/auth.controller.ts
+++ src/backend/features/auth/auth.controller.ts
@@ -1,28 +1,25 @@
 import { inject, injectable } from "tsyringe";
 import { User } from "@/generated/prisma/client";
-import type { IAuthService } from "./auth.service";
-import type { LoginDto, RegisterDto } from "./dto/user.dto";
-import type { IRegistrationStrategy } from "./registration/RegistrationService";
+import type { LoginRequestDTO, RegisterRequestDTO } from "./dto/user.dto";
+import type { IRegistrationStrategy } from "./registration/registration.service";
 import type { ILoginStrategy } from "./login/login.service";
 
 @injectable()
 class AuthController {
 	constructor(
-		@inject("IAuthService") private readonly IAuthService: IAuthService,
 		@inject("RegistrationService")
 		private readonly registrationService: IRegistrationStrategy,
 		@inject("LoginService") private readonly loginService: ILoginStrategy
 	) {}
 
 	async login(
-		loginDto: LoginDto
+		loginDto: LoginRequestDTO
 	): Promise<{ token: string; user: Omit<User, "password"> } | null> {
-		// return await this.IAuthService.login(loginDto);
 		return await this.loginService.login(loginDto);
 	}
 
 	async register(
-		registerDto: RegisterDto
+		registerDto: RegisterRequestDTO
 	): Promise<{ token: string; user: Omit<User, "password"> } | null> {
 		return await this.registrationService.register(registerDto);
 	}
