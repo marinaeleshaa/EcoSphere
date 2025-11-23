83fd7efc0c19575660d03763bb686e02880964dd
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:35:04 +0200

    refactor: remove some unneed vars

--- src/backend/utils/helpers.ts
+++ src/backend/utils/helpers.ts
@@ -3,35 +3,33 @@ import { TokenPayload } from "../interfaces/interfaces";
 import bcrypt from "bcrypt";
 
 export const hashPassword = async (password: string) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+	const salt = await bcrypt.genSalt(10);
+	return await bcrypt.hash(password, salt);
 };
 
 export const comparePassword = async (
-  password: string,
-  hashedPassword: string
+	password: string,
+	hashedPassword: string
 ) => await bcrypt.compare(password, hashedPassword);
 
 export const generateToken = (payload: TokenPayload) => {
-  const secret = process.env.JWT_SECRET as string;
-
-  const options: SignOptions = {
-    expiresIn: process.env.JWT_EXPIRES_IN ? +process.env.JWT_EXPIRES_IN : 7,
-  };
-
-  return jwt.sign(payload, secret, options);
+	return jwt.sign(
+		payload,
+		process.env.JWT_SECRET as Secret,
+		{ expiresIn: process.env.JWT_EXPIRES_IN ?? "7d" } as SignOptions
+	);
 };
 
 export const verifyToken = (token: string) =>
-  jwt.verify(token, process.env.JWT_SECRET as Secret);
+	jwt.verify(token, process.env.JWT_SECRET as Secret);
 
 export const OMIT = <T, K extends keyof T>(
-  obj: T,
-  ...keys: K[]
+	obj: T,
+	...keys: K[]
 ): Omit<T, K> => {
-  const ret = { ...obj };
-  for (const key of keys) {
-    delete ret[key];
-  }
-  return ret;
+	const ret = { ...obj };
+	for (const key of keys) {
+		delete ret[key];
+	}
+	return ret;
 };
