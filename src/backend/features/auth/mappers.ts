574f9f02753e8e6381bb55b56d8a56f3dd4324ba
marwan <marwanabdalmagied@gmail.com>
2025-11-24 02:20:44 +0200

    feat: add 3 new mappers to convert user and shop/rest to public version of them and to convert to token payload

--- src/backend/features/auth/mappers.ts
+++ src/backend/features/auth/mappers.ts
@@ -18,7 +18,7 @@ export const mapRegisterResultToDto = (
 	return {
 		token: command.token,
 		user: {
-			_id: command.user._id!.toString(),
+			_id: command.user._id,
 			email: command.user.email,
 			lastName: command.user.lastName,
 			role: command.user.role,
@@ -26,20 +26,69 @@ export const mapRegisterResultToDto = (
 	};
 };
 
-export const mapUserToPublicProfile = (user: IUser) => {
+export const mapUserToTokenPayload = (user: IUser) => {
 	return {
-		_id: user._id!.toString(),
+		_id: user._id,
 		email: user.email,
 		lastName: user.lastName,
 		role: user.role,
 	};
 };
 
+export const mapRestaurantToTokenPayload = (user: IRestaurant) => {
+	return {
+		_id: user._id,
+		email: user.email,
+		lastName: user.name,
+		role: "shop",
+	};
+};
+
+export const mapUserAsEndUser = (user: IUser) => {
+	return {
+		_id: user._id,
+		email: user.email,
+		lastName: user.lastName,
+		address: user.address || "",
+		avatar: user.avatar || "",
+		birthDate: user.birthDate,
+		phoneNumber: user.phoneNumber,
+		role: "endUser",
+		points: user.points || 0,
+		favePlaces: user.favoritesIds,
+		cart: user.cart,
+		paymentHistory: user.paymentHistory,
+	};
+};
+
+export const mapUserAsOrganizer = (user: IUser) => {
+	return {
+		_id: user._id,
+		email: user.email,
+		lastName: user.lastName,
+		phoneNumber: user.phoneNumber,
+		role: "organizer",
+		subscriptionPeriod: user.subscriptionPeriod,
+		subscribed: user.subscribed,
+		avatar: user.avatar || "",
+		events: user.events,
+	};
+};
+
 export const mapShopToPublicProfile = (shop: IRestaurant) => {
 	return {
-		_id: shop._id!,
+		_id: shop._id,
 		email: shop.email,
 		lastName: shop.name,
 		role: "shop",
+		phoneNumber: shop.phoneNumber,
+		address: shop.location,
+		avatar: shop.avatar || "",
+		openingHours: shop.workingHours,
+		description: shop.description || "",
+		menu: shop.menus,
+		reviews: shop.restaurantRating,
+		subscribed: shop.subscribed,
+		subscriptionPeriod: shop.subscriptionPeriod,
 	};
 };
