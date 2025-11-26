6326d1820271a326e2bb9ae4ff5ad3e41eba8623
marwan <marwanabdalmagied@gmail.com>
2025-11-26 22:07:22 +0200

    refactor: use the rename of login method from auth controller

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,22 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { rootContainer } from "@/backend/config/container";
 import AuthController from "@/backend/features/auth/auth.controller";
 import type {
-  LoginRequestDTO,
-  LoginResponse,
+	LoginRequestDTO,
+	LoginResponse,
 } from "@/backend/features/auth/dto/user.dto";
 import { ApiResponse, ok, serverError } from "@/types/api-helpers";
 
 export const POST = async (
-  request: NextRequest
+	request: NextRequest
 ): Promise<NextResponse<ApiResponse<LoginResponse>>> => {
-  const body = (await request.json()) as LoginRequestDTO;
-  const controller = rootContainer.resolve(AuthController);
+	const body = (await request.json()) as LoginRequestDTO;
+	const controller = rootContainer.resolve(AuthController);
 
-  try {
-    const result = await controller.login(body);
-    return ok(result);
-  } catch (error) {
-    console.log(error);
-    return serverError("Something went wrong");
-  }
+	try {
+		const result = await controller.loginWithCredentials(body);
+		return ok(result);
+	} catch (error) {
+		console.log(error);
+		return serverError("Something went wrong");
+	}
 };
