8895ae5e31adb6aeec5fccefda25a0cf1e1a5f76
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-26 10:53:59 +0200

    fix: separate the login form the main page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,16 +2,14 @@
 import Image from "next/image";
 import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import { FaGoogle, FaFacebookF, FaApple, FaTwitter } from "react-icons/fa";
-import { IoIosArrowRoundForward } from "react-icons/io";
 import SignUp from "@/components/layout/Auth/SignUp";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/frontend/redux/store";
 import { toggleAuthView } from "@/frontend/redux/Slice/AuthSlice";
+import LogIn from "@/components/layout/Auth/LogIn";
 
 const AuthPage = () => {
-  const {active}=useSelector((state:RootState)=>state.auth)
+  const { active } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch<AppDispatch>();
 
   const controls = useAnimation();
@@ -233,8 +231,8 @@ const AuthPage = () => {
     sequence();
   }, [active, controls]);
   const handleToggle = () => {
-        dispatch(toggleAuthView())
-    }
+    dispatch(toggleAuthView());
+  };
   return (
     <section className="relative overflow-hidden bg-background">
       <div className="w-[80%] m-auto">
@@ -251,7 +249,7 @@ const AuthPage = () => {
             }
             transition={{ duration: 2, delay: 0.5 }}
           >
-            <SignUp/>
+            <SignUp />
           </motion.div>
 
           {/* login form */}
@@ -264,88 +262,7 @@ const AuthPage = () => {
             initial={false}
             transition={{ duration: 2, delay: 0.5 }}
           >
-            <p className="capitalize text-center font-extrabold mb-5 text-secondary-foreground text-4xl">
-              Login
-            </p>
-
-            <input
-              type="email"
-              placeholder="Email"
-              className="bg-input text-input-foreground p-3 rounded-full transition duration-300 focus:outline-none pl-10"
-            />
-            <div className="relative">
-              <input
-                type="password"
-                placeholder="Password"
-                className="bg-input text-input-foreground w-full p-3 rounded-full transition duration-300 focus:outline-none pl-10"
-              />
-            </div>
-            {/* forget password */}
-            <Link href="#">
-              <div className="flex justify-end px-5">
-                <div className="flex gap-1 justify-center items-center text-sm group cursor-pointer">
-                  <p className="text-secondary-foreground transition-all duration-300 ">
-                    Forget Password
-                  </p>
-                  <IoIosArrowRoundForward className=" transform transition-all duration-300 ease-out group-hover:translate-x-1 " />
-                </div>
-              </div>
-            </Link>
-
-            <button className="bg-primary text-primary-foreground p-3 rounded-full transition duration-400 hover:scale-102 flex justify-center items-center text-lg gap-2 hover:outline-2 hover:outline-primary hover:outline-offset-4">
-              Login
-              <Image
-                src={"/leaf.png"}
-                width={25}
-                height={25}
-                alt="leaf"
-                className="scale-x-[-1]"
-              />
-            </button>
-
-            {/* divider */}
-            <div className="flex items-center gap-2">
-              <div className="h-px bg-secondary-foreground/50 w-full"></div>
-              <p className="text-stone-500">or</p>
-              <div className="h-px bg-secondary-foreground/50 w-full"></div>
-            </div>
-
-            {/* social login */}
-            <div className="flex justify-evenly items-center my-4 text-4xl text-secondary-foreground ">
-              <Link
-                href={"#"}
-                className="hover:scale-115 hover:shadow-2xl shadow-primary transition duration-300"
-              >
-                <FaGoogle />
-              </Link>
-              <Link
-                href={"#"}
-                className="hover:scale-115 hover:shadow-2xl shadow-primary transition duration-300"
-              >
-                <FaFacebookF />
-              </Link>
-              <Link
-                href={"#"}
-                className="hover:scale-115 hover:shadow-2xl shadow-primary transition duration-300"
-              >
-                <FaApple />
-              </Link>
-              <Link
-                href={"#"}
-                className="hover:scale-115 hover:shadow-2xl shadow-primary transition duration-300"
-              >
-                <FaTwitter />
-              </Link>
-            </div>
-            <p className="text-center text-stone-600 space-x-1 sm:hidden ">
-              <span>New to EcoSphere ?</span>
-              <button
-                onClick={handleToggle}
-                className="text-primary cursor-pointer"
-              >
-                Sign up
-              </button>
-            </p>
+            <LogIn />
           </motion.div>
         </div>
       </div>
