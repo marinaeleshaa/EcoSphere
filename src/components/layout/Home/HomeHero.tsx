3995bc10acd89a52286707050fc55a5385dc8aca
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-27 18:00:21 +0200

    upd: change the hero p color

--- src/components/layout/Home/HomeHero.tsx
+++ src/components/layout/Home/HomeHero.tsx
@@ -18,7 +18,7 @@ const HomeHero = () => {
       <div className="flex justify-center items-center h-screen relative ">
         <div className="relative flex items-center justify-center p-[3%] overflow-hidden h-full ">
           <div className="relative z-10 max-w-md text-center flex flex-col gap-4 justify-center items-center ">
-            <p className="mb-0">#Join the movement</p>
+            <p className="mb-0 text-secondary-foreground">#Join the movement</p>
             <h1 className="text-[clamp(3rem,calc(3rem+2.125*(100vw-23.4375rem)/66.5625),5.125rem)] font-bold leading-none text-foreground">
               Successful fundraisers start here
             </h1>
