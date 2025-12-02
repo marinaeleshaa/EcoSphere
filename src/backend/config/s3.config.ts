d9ee23e76a3339d4eeb54a5fff3a81eeab561c84
marwan <marwanabdalmagied@gmail.com>
2025-12-02 23:10:16 +0200

    refactor: clean up the config and make it singleton

--- src/backend/config/s3.config.ts
+++ src/backend/config/s3.config.ts
@@ -1,22 +1,20 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const bucketRegion = process.env.BUCKET_REGION;
-const accessKey = process.env.AWS_S3_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_S3_SECRET_ACCESS_KEY;
-
-if (!bucketRegion || !accessKey || !secretAccessKey) {
-  throw new Error("Missing AWS S3 configuration in .env file");
+declare global {
+  // prevents duplication in dev hot-reload
+  var _s3: S3Client | undefined;
 }
 
-export const s3Client = new S3Client({
-  region: bucketRegion,
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey,
-  },
-});
+export const s3Client =
+  globalThis._s3 ??
+  new S3Client({
+    region: process.env.BUCKET_REGION!,
+    credentials: {
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY!,
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
+    },
+  });
 
-export const BUCKET_NAME = process.env.BUCKET_NAME;
+if (process.env.NODE_ENV !== "production") globalThis._s3 = s3Client;
+
+export const BUCKET_NAME = process.env.BUCKET_NAME!;
