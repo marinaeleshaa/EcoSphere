ccb684f07cf5a5005be0de083f127b4c263e476b
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-02 10:50:39 +0200

    feat: add partners section to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomeHeroLayout from "@/components/layout/Home/HeroSection/HomeHeroLayout";
 import MarketplaceLayout from "@/components/layout/Home/MarketplaceSection/MarketplaceLayout";
+import PartnerLayout from "@/components/layout/Home/PartnersSection/PartnerLayout";
 
 export default function Home() {
   return (
     <main>
-      <HomeHeroLayout/>
-      <MarketplaceLayout/>
+      <HomeHeroLayout />
+      <MarketplaceLayout />
+      <PartnerLayout />
     </main>
   );
 }
