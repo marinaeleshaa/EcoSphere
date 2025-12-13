0c9a17f5923f3568378e5078d42bbd29ac4446ab
marwan <marwanabdalmagied@gmail.com>
2025-12-13 16:17:36 +0200

    fix: responsive and delete some unwanted seection

--- src/components/layout/Store/ProductCardSection.tsx
+++ src/components/layout/Store/ProductCardSection.tsx
@@ -2,20 +2,22 @@ import ProductCard from "./ProductCard";
 import BasicAnimatedWrapper from "../common/BasicAnimatedWrapper";
 import { IProduct } from "@/types/ProductType";
 
-const ProductCardSection = async ({ products }: { products: Promise<IProduct[]> }) => {
+const ProductCardSection = async ({
+  products,
+}: {
+  products: Promise<IProduct[]>;
+}) => {
   const data = await products;
   return (
-    <section>
-      <div className="w-[80%] mx-auto">
-        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-15">
-          {data.map((product, index) => (
-            <BasicAnimatedWrapper key={product.id} index={index}>
-              <ProductCard {...product} />
-            </BasicAnimatedWrapper>
-          ))}
-        </div>
+    <div className="w-[80%] mx-auto">
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-15">
+        {data.map((product, index) => (
+          <BasicAnimatedWrapper key={product.id} index={index}>
+            <ProductCard {...product} />
+          </BasicAnimatedWrapper>
+        ))}
       </div>
-    </section>
+    </div>
   );
 };
 
