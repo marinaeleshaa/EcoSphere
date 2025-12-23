b4515d98f923b6c2cecf7b967343bec0834add61
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-23 22:16:35 +0200

    fix: update SignUp component to display success message and loader on registration success; swap gender radio button values in UStep2

--- src/components/layout/Auth/SignUp.tsx
+++ src/components/layout/Auth/SignUp.tsx
@@ -13,10 +13,13 @@ import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
 
 type StepKey = "step1" | "step2" | "step3" | "step4";
 
 const SignUp = () => {
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+
   const t = useTranslations("Auth.signup");
   const dispatch = useDispatch<AppDispatch>();
   const {
@@ -60,6 +63,7 @@ const SignUp = () => {
 
     const resultAction = await dispatch(registerUser(registrationData));
     if (registerUser.fulfilled.match(resultAction)) {
+      setRegistrationSuccess(true);
       const response = await signIn("credentials", {
         email: step4Data.email,
         password: step4Data.password,
@@ -69,6 +73,16 @@ const SignUp = () => {
       toast.success(t("successRegister"));
     }
   };
+  if (registrationSuccess) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-4 p-10 text-center">
+        <div className="text-5xl">✅</div>
+        <h2 className="text-2xl font-bold flex gap-2 items-center text-primary">
+          {t("successRegister")} <Loader2 className="animate-spin" />
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <div>
